d121ebc23c67f4aaa9ddcc36732e5da52aa64610
Craig Patik <craig@patik.com>
2025-10-07 20:16:23 +0200

    blog index

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,8 +1,8 @@
-import { withImageDimensions } from '@src/lib/withImageDimensions'
 import fs from 'fs'
-import matter from 'gray-matter'
-import { defaults, omit } from 'lodash'
 import { join } from 'path'
+// import matter from 'gray-matter'
+import { defaults, omit } from 'lodash'
+import { withImageDimensions } from './withImageDimensions'
 
 const postsDirectory = join(process.cwd(), '_posts')
 
@@ -14,60 +14,60 @@ export type PartialPost = {
     [key: string]: string
 }
 
-export async function getPostBySlug(slug: string, fields: string[] = []) {
-    const fullPath = join(postsDirectory, `${slug}/index.md`)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-    const { data, content } = matter(fileContents)
+// export async function getPostBySlug(slug: string, fields: string[] = []) {
+//     const fullPath = join(postsDirectory, `${slug}/index.md`)
+//     const fileContents = fs.readFileSync(fullPath, 'utf8')
+//     const { data, content } = matter(fileContents)
 
-    const partialPost: PartialPost = {}
+//     const partialPost: PartialPost = {}
 
-    // Ensure only the minimal needed data is exposed
-    fields.forEach((field) => {
-        if (field === 'slug') {
-            partialPost[field] = slug
-        } else if (field === 'content') {
-            partialPost[field] = content
-        }
+//     // Ensure only the minimal needed data is exposed
+//     fields.forEach((field) => {
+//         if (field === 'slug') {
+//             partialPost[field] = slug
+//         } else if (field === 'content') {
+//             partialPost[field] = content
+//         }
 
-        if (typeof data[field] !== 'undefined') {
-            partialPost[field] = data[field]
-        }
-    })
+//         if (typeof data[field] !== 'undefined') {
+//             partialPost[field] = data[field]
+//         }
+//     })
 
-    return await withImageDimensions(partialPost)
-}
+//     return await withImageDimensions(partialPost)
+// }
 
 /**
  * Returns a post without the `unpublished` property, unless it was actually requested
  *
  * We do this because we need to inspect the `unpublished` property when deciding which posts to return (based on the environment), but we don't want to actually expose the value unless the caller requested it.
  */
-export function hideUnpublishedValue(requestedFields: string[]): (partialPost: PartialPost) => PartialPost {
-    return (post) => {
-        if (requestedFields.includes('unpublished')) {
-            return post
-        }
+// export function hideUnpublishedValue(requestedFields: string[]): (partialPost: PartialPost) => PartialPost {
+//     return (post) => {
+//         if (requestedFields.includes('unpublished')) {
+//             return post
+//         }
 
-        return omit(post, 'unpublished')
-    }
-}
+//         return omit(post, 'unpublished')
+//     }
+// }
 
-export async function getAllPosts(fields: string[] = []) {
-    const slugs = getPostSlugs()
-    const partialPosts = await Promise.all(
-        slugs.map(async (slug) => await getPostBySlug(slug, [...fields, 'unpublished']))
-    )
+// export async function getAllPosts(fields: string[] = []) {
+//     const slugs = getPostSlugs()
+//     const partialPosts = await Promise.all(
+//         slugs.map(async (slug) => await getPostBySlug(slug, [...fields, 'unpublished'])),
+//     )
 
-    // sort posts by date in descending order
-    partialPosts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+//     // sort posts by date in descending order
+//     partialPosts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
 
-    // Prevent sharing unpublished posts with production builds
-    if (process.env.NODE_ENV === 'production') {
-        return partialPosts.filter((p) => !p.unpublished).map(hideUnpublishedValue(fields))
-    }
+//     // Prevent sharing unpublished posts with production builds
+//     if (process.env.NODE_ENV === 'production') {
+//         return partialPosts.filter((p) => !p.unpublished).map(hideUnpublishedValue(fields))
+//     }
 
-    return partialPosts.map(hideUnpublishedValue(fields))
-}
+//     return partialPosts.map(hideUnpublishedValue(fields))
+// }
 
 export function toCompletePost(items: PartialPost): BlogPost {
     return defaults(
@@ -84,6 +84,6 @@ export function toCompletePost(items: PartialPost): BlogPost {
             coverImage: '',
             ogImage: '',
             content: '',
-        }
+        },
     )
 }
